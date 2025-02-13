f75605d723a8278693d41cb8f4725b99862163a4
Scott-Gibson-Coding <scottchgibson@gmail.com>
2025-02-12 22:33:50 -0800

    Add environment variable

--- src/apps/admin/App.tsx
+++ src/apps/admin/App.tsx
@@ -1,5 +1,17 @@
 import TitleBanner from "../../library/components/ui/TitleBanner";
 
+const LOCATION = import.meta.env.VITE_LOCATION;
+
 export default function AdminApp() {
-  return <TitleBanner>Admin Application</TitleBanner>;
+  return (
+    <>
+      <TitleBanner bannerClassName="inline-block">
+        Admin Application
+      </TitleBanner>
+      <p className="ml-6 inline text-2xl italic">
+        Location read from env:{" "}
+        <span className="font-semibold">{LOCATION}</span>
+      </p>
+    </>
+  );
 }
--- src/apps/consumer/App.tsx
+++ src/apps/consumer/App.tsx
@@ -1,9 +1,17 @@
 import TitleBanner from "../../library/components/ui/TitleBanner";
 
+const LOCATION = import.meta.env.VITE_LOCATION;
+
 export default function ConsumerApp() {
   return (
-    <TitleBanner bannerClassName="bg-blue-300/70">
-      Consumer Application
-    </TitleBanner>
+    <>
+      <TitleBanner bannerClassName="bg-blue-300/70 inline-block">
+        Consumer Application
+      </TitleBanner>
+      <p className="ml-6 inline text-2xl italic">
+        Location read from env:{" "}
+        <span className="font-semibold">{LOCATION}</span>
+      </p>
+    </>
   );
 }
